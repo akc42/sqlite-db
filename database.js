245981f4092a63e4a30092d621b6248282d5c5c7
Alan Chandler <alan@chandlerfamily.org.uk>
2025-09-18 07:43:39 +0100

    Update versions of everything  remove server-utils as not used

--- database.js
+++ database.js
@@ -1,21 +1,21 @@
 /**
 @licence
-    Copyright (c) 2020 Alan Chandler, all rights reserved
+    Copyright (c) 2020-2025 Alan Chandler, all rights reserved
 
-    This file is part of Meeting.
+    This file is part of Sqlite-db
 
-    Meeting is free software: you can redistribute it and/or modify
+    Sqlite-db is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Meeting is distributed in the hope that it will be useful,
+    Sqlite-db is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Meeting.  If not, see <http://www.gnu.org/licenses/>.
+    along with Sqlite-db.  If not, see <http://www.gnu.org/licenses/>.
 */
 import Database from 'better-sqlite3';
 import Debug from 'debug';
@@ -54,7 +54,11 @@ export default function (dbfilename, initfile) {
     }
 
     db.pragma('foreign_keys = ON');
-    process.on('exit', () => db.close());
+    process.on('exit', () => {
+      let tmp = db;
+      db = null;
+      tmp.close()
+    });
   }
   return db;
 } 
